d394f5e561181f644134002b979414e416d121e5
Aslan Sarı <aslansari.dev@gmail.com>
2022-12-18 13:48:02 +0300

    chore: add compose metrics, update gradle plugin

--- app/src/main/java/com/aslansari/pokedeck/PokeDeckApp.kt
+++ app/src/main/java/com/aslansari/pokedeck/PokeDeckApp.kt
@@ -4,12 +4,12 @@ import android.app.Application
 import com.aslansari.pokedeck.network.PokemonService
 import com.aslansari.pokedeck.network.RetrofitClient
 import com.aslansari.pokedeck.repository.PokemonRepository
-import com.aslansari.pokedeck.viewmodel.PokemonViewModel
 
 class PokeDeckApp: Application() {
 
-    private val pokemonAPIBaseURL = "https://pokeapi.co/api/v2/"
-    val pokemonService by lazy { RetrofitClient.getClient(baseUrl = pokemonAPIBaseURL).create(PokemonService::class.java) }
+    val pokemonService by lazy {
+        RetrofitClient.getClient(baseUrl = BuildConfig.API_BASE_URL).create(PokemonService::class.java)
+    }
     val pokemonRepository by lazy { PokemonRepository(pokemonService) }
 
 }
