856db2a5ba8d09cefad02ae7826c6b3223b66913
Aslan Sarı <aslansari.dev@gmail.com>
2022-12-18 13:52:11 +0300

    feat: add dependency injection library hilt

--- app/src/main/java/com/aslansari/pokedeck/PokeDeckApp.kt
+++ app/src/main/java/com/aslansari/pokedeck/PokeDeckApp.kt
@@ -4,7 +4,9 @@ import android.app.Application
 import com.aslansari.pokedeck.network.PokemonService
 import com.aslansari.pokedeck.network.RetrofitClient
 import com.aslansari.pokedeck.repository.PokemonRepository
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class PokeDeckApp: Application() {
 
     val pokemonService by lazy {
